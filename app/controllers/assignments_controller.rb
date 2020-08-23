3207bb74991dd24a12506ed0106d81857ef211ea
LetsJessCode <letsjesscode@gmail.com>
2020-08-23 12:50:08 -0400

    sorted thru the create assignment for user

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,27 +1,39 @@
 class AssignmentsController < ApplicationController
-    before_action :this_assignment, except: [:index, :new, :create]
+    before_action :this_assignment, only: [:show, :edit, :update, :destroy]
+    
 
     def index
-        @assignments = Assignment.all
-        @user = User.find_by_id(params[:id])
+        @assignments = current_user.assignments.all
     end
 
     def new
-
+        @assignment = Assignment.new
     end
 
     def create
-
-    end
+        #  binding.pry
+     @assignment = current_user.assignments.build(assignment_params)
+        if @assignment.save
+            # binding.pry
+          redirect_to user_assignments_path(current_user, @assignment)
+        else
+          render :new
+      end
+  end
 
     def show
 
     end
 
     def edit
-       @user = User.find_by_id(params[:id])
-       @course = Course.find_by_id(params[:id])
+      binding.pry
+      current_user
+      if @assignment && @assignment.id == current_user
+          redirect_to user_edit_assignment_path(current_user, @assignment)
+      else
+        render :edit
     end
+  end 
 
     def update
 
@@ -33,10 +45,10 @@ class AssignmentsController < ApplicationController
 
   private
     def assignment_params
-        params.require(:assignment).permit(:name, :comment, :due, :completed, :user_id, :course_id)
+        params.require(:assignment).permit(:name, :comment, :due, :completed, :user_id, :course_id, course_attributes: [:name] )
     end
 
     def this_assignment
-         Assignment.find_by_id(params[:id])
+         current_user.assignments.find_by_id(params[:id])
     end
 end
